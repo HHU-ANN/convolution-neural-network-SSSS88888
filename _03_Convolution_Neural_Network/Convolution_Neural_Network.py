0214d672793efc63c37500c702b7eaedf9f6310d
= <=>
2023-06-23 16:15:15 +0800

    第一次提交

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -1,4 +1,4 @@
-# 在该文件NeuralNetwork类中定义你的模型 
+n# 在该文件NeuralNetwork类中定义你的模型 
 # 在自己电脑上训练好模型，保存参数，在这里读取模型参数（不要使用JIT读取），在main中返回读取了模型参数的模型
 
 import os
@@ -38,7 +38,7 @@ class ResidualBlock(nn.Module):
         return out
     
 class NeuralNetwork(nn.Module):
-    # ResNet定义
+    # ResNet
     def __init__(self, block, layers, num_classes=10):
         super(NeuralNetwork, self).__init__()
         self.in_channels = 16
